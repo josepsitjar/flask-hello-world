307431b55ef41181f9bf7bb415551809416d259d
josepsitjar <josepsitjar@gmail.com>
2024-11-25 11:40:32 +0100

    create db

--- app/mapa/mapa.py
+++ app/mapa/mapa.py
@@ -1,7 +1,16 @@
 from flask import Blueprint 
 
 mapa_bp = Blueprint("mapa", __name__)
+from ..database import db 
 
 @mapa_bp.route("/")
 def home():
     return '<h1>Mi primer blueprint</h1>'
+
+@mapa_bp.route("/inicializar_db")
+def inicializar_db():
+    try:
+        db.create_all()
+        return "La base de datos se ha creado correctamente.", 200
+    except Exception as e: 
+        return f"Ha ocurrido un error: {e}", 500
