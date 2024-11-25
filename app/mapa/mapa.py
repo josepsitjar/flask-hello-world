fb3a31c3bacdc607cc7a5c2e76df570aec7b4c9f
josepsitjar <josepsitjar@gmail.com>
2024-11-25 15:02:17 +0100

    mapa js

--- app/mapa/mapa.py
+++ app/mapa/mapa.py
@@ -2,7 +2,12 @@ from flask import Blueprint
 from ..database import db 
 from flask import Blueprint, render_template
 
-mapa_bp = Blueprint("mapa", __name__, template_folder='templates')
+mapa_bp = Blueprint("mapa", 
+                    __name__, 
+                    template_folder='templates',
+                    static_folder='static',
+                    url_prefix='/mapa'
+                    )
 
 @mapa_bp.route("/")
 def home():
