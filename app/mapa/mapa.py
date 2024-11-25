f8f2b79832e9d4eefb984944355cec753771815b
josepsitjar <josepsitjar@gmail.com>
2024-11-25 16:59:04 +0100

    endpoint estaciones

--- app/mapa/mapa.py
+++ app/mapa/mapa.py
@@ -1,6 +1,9 @@
 from flask import Blueprint
 from ..database import db 
 from flask import Blueprint, render_template
+from shapely_geojson import dumps, Feature, FeatureCollection
+from geoalchemy2.shape import to_shape
+from ..models import Estacion
 
 mapa_bp = Blueprint("mapa", 
                     __name__, 
@@ -12,3 +15,19 @@ mapa_bp = Blueprint("mapa",
 @mapa_bp.route("/")
 def home():
     return render_template('mapa/vista.html')
+
+
+@mapa_bp.route("/estaciones")
+def estaciones_geojson():
+    """Función para crear un archivo GeoJson a partir de los datos del modelo Estacion"""
+
+    estaciones = []
+    for estacion in Estacion.query.all():
+        geometry = estacion.geom
+        feature = Feature(to_shape(geometry), properties={'key': 'value'})
+        estaciones.append(feature)
+
+    feature_collection = FeatureCollection(estaciones)
+    geoJson = dumps(feature_collection)
+
+    return geoJson
